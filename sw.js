9a380d8b935ff3851151dcdba7496a9819a14b35
marcricon06 <marc.ricon@estudiantat.upc.edu>
2025-06-24 21:40:04 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'capital-eu-cache-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  // si tienes otros archivos, agrégalos aquí
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
